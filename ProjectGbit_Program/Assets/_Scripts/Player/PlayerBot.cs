7c0b4b987fdc018b77b77994298959b456634daa
7Chords <126320606+7Chords@users.noreply.github.com>
2024-07-20 01:05:51 +0800

    1

---
+++ ProjectGbit_Program/Assets/_Scripts/Player/PlayerBot.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBot : MonoBehaviour
+{
+    public float moveSpeed;
+
+    private Rigidbody _rigidbody;
+
+    public InputSetting inputSetting;
+
+    public Player player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player._isPlayerSetup)
+        {
+            Movement(inputSetting.inputDir);
+        }
+        
+    }
+    
+    /// <summary>
+    /// 玩家移动方法
+    /// </summary>
+    /// <param name="inputDir"></param>
+    private void Movement(Vector2 inputDir)
+    {
+        Vector3 movement = new Vector3(inputDir.x, 0, inputDir.y).normalized * moveSpeed ;
+        // 移动玩家
+        _rigidbody.velocity = new Vector3(movement.x, _rigidbody.velocity.y, movement.z);
+    }
+}
