91c98892b5c4c1023d8e01c38754afc6875fefb4
Jerry <75481387+Jerry343Games@users.noreply.github.com>
2024-07-20 00:56:36 +0800

    选人界面，角色功能复写

--- ProjectGbit_Program/Assets/_Scripts/Player/Player.cs
+++ ProjectGbit_Program/Assets/_Scripts/Player/Player.cs
@@ -35,27 +35,16 @@ public class Player : MonoBehaviour
     public GameObject[] playerModel;
     
     /// <summary>
-    /// 移动速度
-    /// </summary>
-    [Header("移动速度")]
-    public float moveSpeed;
-    
-    /// <summary>
-    /// 玩家刚体
-    /// </summary>
-    private Rigidbody _rigidbody;
-
-    /// <summary>
     /// 玩家是否完成设置
     /// </summary>
-    private bool _isPlayerSetup;
+    [HideInInspector]
+    public bool _isPlayerSetup;
     void Awake()
     {
         _isPlayerSetup = false;
         _playerInput = gameObject.GetComponent<PlayerInput>();
         _multiplayerEventSystem =gameObject.GetComponent<MultiplayerEventSystem>();
         _inputSetting = GetComponent<InputSetting>();
-        _rigidbody = GetComponent<Rigidbody>();
     }
 
     void Start()
@@ -65,21 +54,7 @@ public class Player : MonoBehaviour
     
     void Update()
     {
-        if (_isPlayerSetup)
-        {
-            Movement(_inputSetting.inputDir);
-        }
-    }
 
-    /// <summary>
-    /// 玩家移动方法
-    /// </summary>
-    /// <param name="inputDir"></param>
-    private void Movement(Vector2 inputDir)
-    {
-        Vector3 movement = new Vector3(inputDir.x, 0, inputDir.y).normalized * moveSpeed ;
-        // 移动玩家
-        _rigidbody.velocity = new Vector3(movement.x, _rigidbody.velocity.y, movement.z);
     }
     
     /// <summary>
