c24a8680b8288158aaf46abb0209d49a890dfa26
7Chords <126320606+7Chords@users.noreply.github.com>
2024-07-20 10:13:13 +0800

    警察初步完成

--- ProjectGbit_Program/Assets/_Scripts/Player/PlayerPolice.cs
+++ ProjectGbit_Program/Assets/_Scripts/Player/PlayerPolice.cs
@@ -11,12 +11,37 @@ public class PlayerPolice : MonoBehaviour
     public InputSetting inputSetting;
 
     public Player player;
-    // Start is called before the first frame update
+
+    [Header("下QTE命令参数")]
+    public float CallQTECD;//下QTE命令的cd
+
+    private float _callQTECDTimer;
+
+    [Header("扫描参数")]
+    public float ScanBotDuration;//扫描持续时间
+
+    private float _scanBotTimer;
+
+    public float ScanBotCD;//扫描CD
+
+    private float _scanBotCDTimer;
+
+    public float CheckPlayerBotDistance;//扇形扫描区域弦长
+
+    public float CheckPlayerBotAngle;//扇形扫描区域角度
+
+    private bool _isBeingScan;
+
+    private bool _canStartScan;
+
+    [Header("攻击参数")]
+    public float AttackCD;
+
+    private float _attackTimer;
+
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
-
-        Invoke("CallQTE",5f);
     }
 
     // Update is called once per frame
@@ -30,6 +55,16 @@ public class PlayerPolice : MonoBehaviour
         {
             CallQTE();
         }
+        if(inputSetting.isPressScan)
+        {
+            StartScanPlayerBot();
+        }
+        if(inputSetting.isPressSwitch)
+        {
+            Attack();
+        }
+        SkillCDUpdate();
+        ScanPlayerBot();
     }
 
     /// <summary>
@@ -43,14 +78,53 @@ public class PlayerPolice : MonoBehaviour
         _rigidbody.velocity = new Vector3(movement.x, _rigidbody.velocity.y, movement.z);
     }
 
+    /// <summary>
+    /// 技能CD计时器
+    /// </summary>
+    private void SkillCDUpdate()
+    {
+        //QTECD计时
+        if(_callQTECDTimer>0)
+        {
+            _callQTECDTimer -= Time.deltaTime;
+        }
+        //扫描时间计时
+        if (_scanBotTimer>0)
+        {
+            _scanBotTimer -= Time.deltaTime;
+        }
+        else
+        {
+            _isBeingScan = false;
+        }
+        //扫描CD计时
+        if (_scanBotCDTimer > 0)
+        {
+            _scanBotCDTimer -= Time.deltaTime;
+        }
+        else
+        {
+            _canStartScan = true;
+        }
+        //攻击CD计时
+        if(_attackTimer>0)
+        {
+            _attackTimer -= Time.deltaTime;
+        }
+    }
+
 
     /// <summary>
     /// 使用QTE技能
     /// </summary>
     private void CallQTE()
     {
+        if(_callQTECDTimer > 0)
+        {
+            return;
+        }
         Debug.Log("警察开始qte");
-        //if (SceneManager.Instance.AIBotList.Count == 0) return;
+
         foreach(var aiBot in SceneManager.Instance.AIBotList)
         {
             aiBot.GetComponent<AIBot>().ExecuteQTE();
@@ -59,6 +133,81 @@ public class PlayerPolice : MonoBehaviour
         {
             playerBot.GetComponent<PlayerBot>().BeginQTE();
         }
+    }
+
+
+    /// <summary>
+    /// 开始扫描机器人
+    /// </summary>
+    private void StartScanPlayerBot()
+    {
+        if(_canStartScan)
+        {
+            _isBeingScan = true;
+
+            _canStartScan = false;
+
+            _scanBotTimer = ScanBotDuration;
+
+            _scanBotCDTimer = ScanBotCD;
+        }
+    }    
+
+    /// <summary>
+    /// 扫描机器人技能
+    /// </summary>
+    private void ScanPlayerBot()
+    {
+        if(!_isBeingScan)
+        {
+            return;
+        }
+
+
+        Collider[] colliders = Physics.OverlapSphere(transform.position, CheckPlayerBotDistance);
+        foreach (Collider collider in colliders)
+        {
+
+            if (collider.CompareTag("Player"))
+            {
+                Vector3 directionToPlayer = collider.transform.position - transform.position;
+                float angle = Vector3.Angle(transform.forward, directionToPlayer);
+
+                if (angle < CheckPlayerBotAngle / 2f)
+                {
+                    Debug.Log("扫描出玩家了");
+                }
+            }
+
+        }
+    }
+
+    /// <summary>
+    /// 警察攻击
+    /// </summary>
+    private void Attack()
+    {
+        if(_attackTimer>0)
+        {
+            return;
+        }
+        Debug.Log("玩家发动了攻击");
+
+    }
+
+    /// <summary>
+    /// 辅助线绘制
+    /// </summary>
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, CheckPlayerBotDistance);
+
+        Vector3 viewAngleA = Quaternion.AngleAxis(CheckPlayerBotAngle / 2, transform.up) * transform.forward;//扇形视野射线初始角度
+        Vector3 viewAngleB = Quaternion.AngleAxis(-CheckPlayerBotAngle / 2, transform.up) * transform.forward;//扇形视野射线终止角度
+
+
+        Gizmos.DrawLine(transform.position, transform.position + viewAngleA * CheckPlayerBotDistance);//起点，方向*长度
+        Gizmos.DrawLine(transform.position, transform.position + viewAngleB * CheckPlayerBotDistance);//同上
     }
 
 }
