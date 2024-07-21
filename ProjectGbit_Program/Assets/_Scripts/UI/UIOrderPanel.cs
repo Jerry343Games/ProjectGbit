7239bdce1fcde9a3139fc85d14c8339a90a74214
Jerry <75481387+Jerry343Games@users.noreply.github.com>
2024-07-21 11:50:29 +0800

    警察命令UI，计时UI，任务面板

---
+++ ProjectGbit_Program/Assets/_Scripts/UI/UIOrderPanel.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIOrderPanel : MonoBehaviour
+{
+
+    public Image headImg;
+    public Image dialoguePanel;
+    public TMP_Text dialogueText;
+
+    public int countDownVale=3;
+
+    private float _startFontSize;
+    private float _numberFountSize;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ShowPanel();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// 显示对话框
+    /// </summary>
+    public void ShowPanel()
+    {
+        Sequence sequence = DOTween.Sequence();
+        sequence.Append(headImg.rectTransform.DOScale(1, 0.2f));
+        sequence.Append(dialoguePanel.rectTransform.DOScale(1, 0.3f));
+        sequence.AppendInterval(0.5f).OnComplete(() =>
+        {
+            SayWords();
+        });
+    }
+
+    /// <summary>
+    /// 收起对话框
+    /// </summary>
+    public void ClosePanel()
+    {
+        Sequence sequence = DOTween.Sequence();
+        sequence.Append(dialoguePanel.rectTransform.DOScale(0, 0.3f));
+        sequence.Append(headImg.rectTransform.DOScale(0, 0.2f));
+        dialogueText.fontSize = _startFontSize;
+    }
+
+    public void SayWords()
+    {
+        StartCoroutine(CountdownCoroutine());
+    }
+    
+    IEnumerator CountdownCoroutine()
+    {
+        while (countDownVale >= 0)
+        {
+            dialogueText.fontSize = _numberFountSize;
+            dialogueText.text = countDownVale.ToString();
+            yield return new WaitForSeconds(1); // 等待1秒
+            countDownVale--;
+        }
+
+        // 倒计时结束后，可以在这里执行其他逻辑
+        ClosePanel();
+    }
+}
